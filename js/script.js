d77d121d720f7ec76d4adece685288584fff389d
c0braBubbles <233566@usn.no>
2022-11-13 15:50:48 +0100

    Contact-side og objekt logikk

---
+++ js/script.js
@@ -0,0 +1,8 @@
+class Media {
+    constructor(path, name, headerTxt, subTxt) {
+        this.path = path; 
+        this.name = name; 
+        this.headerTxt = headerTxt; 
+        this.subTxt = subTxt;
+    }   
+}
